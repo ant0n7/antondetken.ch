0fd29fe9aff1b364eb9408f9e84fc2b192fe38a9
andrinklarer <andrin.klarer@noseryoung.ch>
2021-08-06 15:49:18 +0200

    minified

--- js/logoswitch.js
+++ js/logoswitch.js
@@ -1,15 +1,1 @@
-
-    function myFunction(x) {
-  if (x.matches) { 
-    document.querySelector("#anton_img-nav").setAttribute("style", "display:block");
-    document.querySelector("#anton_text-nav").setAttribute("style", "display:none");
-
-  } else {
-    document.querySelector("#anton_img-nav").setAttribute("style", "display:none");
-    document.querySelector("#anton_text-nav").setAttribute("style", "display:block");
-  }
-}
-
-var x = window.matchMedia("(max-width: 992px)")
-myFunction(x) 
-x.addEventListener("change", myFunction) 
+function myFunction(t){t.matches?(document.querySelector("#anton_img-nav").setAttribute("style","display:block"),document.querySelector("#anton_text-nav").setAttribute("style","display:none")):(document.querySelector("#anton_img-nav").setAttribute("style","display:none"),document.querySelector("#anton_text-nav").setAttribute("style","display:block"))}var x=window.matchMedia("(max-width: 992px)");myFunction(x),x.addEventListener("change",myFunction);
